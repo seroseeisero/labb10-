d1d41f7da8dcf29b585a0ef8d52b82a68a97dd05
Pongpx <yannafong555@gmail.com>
2024-12-20 14:44:04 +0700

    LAB 10 DONE

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,26 +2,45 @@
 using namespace std;
 
 int main(){
-	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
-	cout << "Please input grade of each student (A-F) or input 0 to exit.";
+	int count[5] = {};
+	string grade;
+	int n = 1;
+	cout << "Please input grade of each student (A-F) or input 0 to exit.\n";
 	do{
-		cout << "Student [" << "]: ";
-		cin >> grade; //The loop must be terminated when grade = '0'
-		if(true) // if grade is A
-			//Do something
-		}else if(true) // if grade is B
-			//Do something
-		//and so on ... for grade = C, D, F	
-		}else{ // grade is wrong input
-			//Do something
+		cout << "Student [" << n << "]: ";
+		cin >> grade; 
+		if(grade == "0") { 
+		    break;
+		}
+		
+		if(grade == "A"){ // if grade is A
+			count[0]++;
+			n++;
+		}else if(grade == "B"){ // if grade is B
+			count[1]++;
+			n++;
+		}else if(grade == "C"){
+		    count[2]++;
+		    n++;
+		}else if(grade == "D"){
+		    count[3]++;
+		    n++;
+		}else if(grade == "F"){
+		    count[4]++;
+		    n++;
+		}
+		else{
+	        cout << "Wrong input. Please input again.\n";
 		} 
-	}while(true);
+	}while(grade != "0");
 	
 	
-	cout << "In total ? students.";
+	cout << "In total "<< n-1 << " students.\n";
 	cout << "A = " << count[0] <<", ";
-	cout << "B = " << count[1] <<", ";	
-	//	and so on ... for grade = C, D, F	
+	cout << "B = " << count[1] <<", ";
+	cout << "C = " << count[2] <<", ";
+	cout << "D = " << count[3] <<", ";
+	cout << "F = " << count[4];
 	
 	return 0;
 }
--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,24 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+int main(){
+    ifstream sr;
+    sr.open("score.txt");
+    double sum,Avg,sum1 = 0;
+    int count = 0;
+    string tl;
+    while(getline(sr,tl)){
+        sum += atof(tl.c_str());
+        sum1 += pow(atof(tl.c_str()),2);
+        count++;
+    }
+    Avg = sum/count;
+    double Sd = sqrt((sum1/count)-pow(Avg,2));
+    cout << "Number of data = "<< count << "\n";
+    cout << setprecision(3);
+    cout << "Mean = "<< Avg<<"\n";
+    cout << "Standard deviation = "<< Sd;
+}
